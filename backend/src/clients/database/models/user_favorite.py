27efa2beb3abcc6e5ffd0bf217c9aaf24ba4bd14
d.drik <27danik98@gmail.com>
2025-04-26 15:03:18 +0300

    add status for user favorite anime

--- backend/src/clients/database/models/user_favorite.py
+++ backend/src/clients/database/models/user_favorite.py
@@ -1,11 +1,12 @@
 from src.clients.database.base import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import BigInteger, ForeignKey
+from sqlalchemy import BigInteger, ForeignKey, String
 
 class UserFavorite(Base):
     __tablename__ = "user_favorites"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    status: Mapped[str] = mapped_column(String(255), nullable=True)
     anime_id: Mapped[BigInteger] = mapped_column(ForeignKey("anime.id"))
     user_id: Mapped[BigInteger] = mapped_column(ForeignKey("users.id"))
 
--- backend/src/services/user_favorite/schemas.py
+++ backend/src/services/user_favorite/schemas.py
@@ -1,10 +1,24 @@
+from enum import StrEnum
+
 from pydantic import BaseModel
 
+
+class AnimeStatus(StrEnum):
+    PLANNED = "planned"
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
+    CANCELED = "canceled"
+    DELAYED = "delayed"
+    REWATCHING = "rewatching"
+
+
 class CreateUserFavoriteSchema(BaseModel):
-    anime_id: int | None = None
-    user_id: int | None = None
+    anime_id: int
+    user_id: int
+    status: AnimeStatus | None = None
 
 
 class UpdateUserFavoriteSchema(BaseModel):
     anime_id: int | None = None
     user_id: int | None = None
+    status: AnimeStatus | None
