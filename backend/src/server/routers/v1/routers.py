47a4e72f3aa2b8ea5186b8f3d9f6173991596ab6
d.drik <27danik98@gmail.com>
2025-05-02 20:00:10 +0300

    add crud path

--- backend/src/server/routers/v1/routers.py
+++ backend/src/server/routers/v1/routers.py
@@ -7,10 +7,13 @@ from src.server.routers.v1.anime_review_router import anime_review_router
 from src.server.routers.v1.genre_router import genre_router
 from src.server.routers.v1.user_anime_status_router import user_anime_status_router
 
+crud_router = APIRouter(prefix="/crud")
+crud_router.include_router(anime_router)
+crud_router.include_router(anime_genre_router)
+crud_router.include_router(anime_review_router)
+crud_router.include_router(genre_router)
+crud_router.include_router(user_anime_status_router)
+
 api_v1_router = APIRouter(prefix="/api/v1")
+api_v1_router.include_router(crud_router)
 api_v1_router.include_router(user_router)
-api_v1_router.include_router(anime_router)
-api_v1_router.include_router(anime_genre_router)
-api_v1_router.include_router(anime_review_router)
-api_v1_router.include_router(genre_router)
-api_v1_router.include_router(user_anime_status_router)
