fea7e9760e394f1a47e8a22a97debf0e9bccc881
d.drik <27danik98@gmail.com>
2025-05-14 18:38:43 +0300

    add field username

--- backend/src/clients/database/models/user.py
+++ backend/src/clients/database/models/user.py
@@ -1,5 +1,6 @@
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
-from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, relationship, mapped_column
 
 from src.clients.database.base import Base
 from src.clients.database.models.anime_rating import AnimeRating
@@ -8,6 +9,10 @@ from src.clients.database.models.user_anime_status import UserAnimeStatus
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
     __tablename__ = "users"
+
+    username: Mapped[str] = mapped_column(
+            String(length=320), unique=True, nullable=False
+    )
 
     ratings: Mapped[list["AnimeRating"]] = relationship(
         "AnimeRating", back_populates="user", cascade="all, delete-orphan"
