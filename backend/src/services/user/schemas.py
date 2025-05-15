b711eb55bfe8d096f33167127617020682c38de3
d.drik <27danik98@gmail.com>
2025-05-15 20:07:31 +0300

    email null to schema

--- backend/src/services/user/schemas.py
+++ backend/src/services/user/schemas.py
@@ -1,14 +1,17 @@
 import uuid
 
 from fastapi_users import schemas
+from pydantic import EmailStr, Field
 
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
     username: str
+    email: EmailStr | None
 
 
 class UserCreate(schemas.BaseUserCreate):
     username: str
+    email: EmailStr | None = Field(None)
 
 
 class UserUpdate(schemas.BaseUserUpdate):
