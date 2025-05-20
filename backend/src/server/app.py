835b034f7e7413940733d4d930ffd3a66894639c
usagi <solevoi.ghoul@gmail.com>
2025-05-20 20:43:07 +0300

    change allow origins CORS

--- backend/src/server/app.py
+++ backend/src/server/app.py
@@ -14,7 +14,7 @@ class CustomFastAPI(FastAPI):
     container: DependencyContainer
 
 
-origins = ["http://localhost:5173", "http://127.0.0.1:5173", "http://0.0.0.0:8000"]
+origins = ["http://localhost:5174", "http://127.0.0.1:5174", "http://0.0.0.0:8000"]
 
 
 @asynccontextmanager
@@ -29,7 +29,7 @@ def create_application() -> CustomFastAPI:
     server.add_middleware(CacheMiddleware, redis_cache=container.redis_cache())
     server.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
