8784aacf44040b67a93a5e9be1d48c4c88c01f29
d.drik <27danik98@gmail.com>
2025-05-28 01:34:28 +0300

    add big text for anime

--- backend/src/clients/database/models/anime.py
+++ backend/src/clients/database/models/anime.py
@@ -1,4 +1,4 @@
-from sqlalchemy import SmallInteger, String
+from sqlalchemy import SmallInteger, String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.clients.database.base import Base
@@ -10,7 +10,7 @@ class Anime(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(255), nullable=False)
-    description: Mapped[str] = mapped_column(nullable=True)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
     image_url: Mapped[str] = mapped_column(String(255), nullable=True)
     rating: Mapped[float] = mapped_column(nullable=True)
     type: Mapped[str] = mapped_column(String(255), nullable=True)
