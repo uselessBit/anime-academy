fc89184e12aa8c27db992ea1de8ad082ec8a9ee1
usagi <solevoi.ghoul@gmail.com>
2025-06-09 21:11:09 +0300

    minor change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
@@ -9,7 +8,7 @@ export default defineConfig({
         port: 5174,
         proxy: {
             '/api': {
-                target: 'https://shawa-bear-tg-mini-app.onrender.com/',
+                target: 'http://localhost:8000',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ''),
                 secure: false,
